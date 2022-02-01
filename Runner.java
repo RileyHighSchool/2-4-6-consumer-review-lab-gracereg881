0d4d90f3e4b72e4ba86bb2b41a96d669fe9739e8
Grace Regulinski <grace81864@students.sbcsc.k12.in.us>
2022-02-01 11:04:24 -0500

    activity 2 not done

--- Runner.java
+++ Runner.java
@@ -4,5 +4,9 @@ public class Runner {
     public static void main(String[] args)
     {
         System.out.println(Review.sentimentVal("cold"));
+
+        double num = Review.sentimentVal("warm");
+
+        System.out.println(Review.totalSentiment(fileName));
     }
 }
